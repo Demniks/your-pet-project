f8c6554dda94a23cf2ff94244719c0420ed65be9
Демченко Сергей <Demniks@ukr.net>
2023-09-06 21:01:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/project-savePets05">
+      <BrowserRouter basename="/your-pet-project">
         <GlobalStyles />
         <App />
       </BrowserRouter>
